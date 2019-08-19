265e90375f2941031fe6d6879518a46eba5b1aff
yeyuning1 <yeyuning1@gmail.com>
2019-08-19 17:06:42 +0800

    配置、API、蓝图

--- 配置、API、蓝图/home/views.py
+++ 配置、API、蓝图/home/views.py
@@ -1,7 +1,16 @@
+import re
+
 from flask_restful import Resource, inputs
 from flask_restful.reqparse import RequestParser
 
 from home.utils import decorator1, decorator2
+
+
+def func1(value):
+    if re.match(r'^1[3-9]\d{9}$', value):
+        return value
+    else:
+        raise ValueError('格式不匹配')
 
 
 class FirstResource(Resource):
@@ -23,7 +32,7 @@ class FirstResource(Resource):
                             case_sensitive=False,
                             trim=True,
                             nullable=True)
-        parser.add_argument('age')
+        parser.add_argument('age', type=func1)
         args = parser.parse_args()
         name = args.username
         age = args.age
