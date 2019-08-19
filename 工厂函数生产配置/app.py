ade0cde4c6bcd7fca880f62982bc7faa371ea276
yeyuning1 <yeyuning1@gmail.com>
2019-08-19 15:20:15 +0800

    工厂函数 生产配置

---
+++ 工厂函数生产配置/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from config import config_dict
+
+
+def index():
+    pass
+
+
+def create_app(config_type):
+    app = Flask(__name__)
+    config_class = config_dict.get(config_type)
+    app.config.from_object(config_class)
+    app.config.from_envvar('MY_ENV', silent=True)
+    app.add_url_rule('/', endpoint='index', view_func=index)
+    return app
