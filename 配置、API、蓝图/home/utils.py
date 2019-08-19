b7af9012266515a96696c76aaeddbb578a7b9738
yeyuning1 <yeyuning1@gmail.com>
2019-08-19 16:03:10 +0800

    配置、API、蓝图

---
+++ 配置、API、蓝图/home/utils.py
@@ -0,0 +1,17 @@
+from functools import wraps
+
+
+def decorator1(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def decorator2(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return wrapper
--- 配置、API、蓝图/home/views.py
+++ 配置、API、蓝图/home/views.py
@@ -1,8 +1,22 @@
 from flask_restful import Resource
+from flask_restful.reqparse import RequestParser
+
+from home.utils import decorator1, decorator2
 
 
 class FirstResource(Resource):
+    method_decorators = {
+        'get': decorator1,
+        'post': decorator2
+    }
+
     def get(self):
+        parser = RequestParser()
+        parser.add_argument('name')
+        parser.add_argument('age')
+        args = parser.parse_args()
+        args.name
+        args.age
         return {'get': 'foo'}
 
     def post(self):
