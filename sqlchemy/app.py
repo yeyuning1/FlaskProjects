b8737b1736d87f53f968c3d88dc08ec422e5a16e
yeyuning1 <yeyuning1@gmail.com>
2019-08-22 02:01:43 +0800

    sqlalchemy

--- sqlchemy/app.py
+++ sqlchemy/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@192.168.42.80/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1/test'
 # 设置是否追踪数据库变化   一般不会开启, 影响性能
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # 设置是否打印底层执行的SQL语句
