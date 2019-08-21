29405d7060daeae48260a4cd7742971e183064d1
yeyuning1 <yeyuning1@gmail.com>
2019-08-21 20:26:59 +0800

    sqlalchemy

--- sqlchemy/app.py
+++ sqlchemy/app.py
@@ -10,10 +10,23 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    __tablename__ = 't_user'  # 设置表名，默认为类名小写
+    id = db.Column(db.Integer, primary_key=True)  # 设置为主键
+    name = db.Column(db.String(20), unique=True)  # 设置为唯一
+    age = db.Column(db.Integer)
+
+
 @app.route('/')
 def hello_world():
+    db.drop_all()
+    db.create_all()
+
+
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+
+
+    app.run(debug=True)
