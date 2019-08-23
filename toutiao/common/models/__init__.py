87db126e1693a526fa2296bc57dd6a56c53f476c
yeyuning1 <yeyuning1@gmail.com>
2019-08-23 20:48:43 +0800

    项目初始化

--- toutiao/common/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# from flask_sqlalchemy import SQLAlchemy
-from .db_routing.routing_sqlalchemy import RoutingSQLAlchemy
-
-# 创建数据库连接对象
-db = RoutingSQLAlchemy()
--- toutiao/common/script/celery_sms.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-source ~/.bash_profile
-cd /home/python/toutiao-backend/common
-workon toutiao
-exec celery -A celery_tasks.main worker -l info -Q sms
